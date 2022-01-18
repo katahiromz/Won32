d5850a83428f9e3e118f144e3bc8595ff6fce84f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2022-01-18 22:02:38 +0900

    add Won32.h

---
+++ Won32.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef _INC_WINDOWS
+    #include <windows.h>
+#endif
+
+#include "WonAlphaBlend/WonAlphaBlend.h"
+#include "WonFileID/WonFileID.h"
+#include "WonGetDiskFreeSpaceEx/WonGetDiskFreeSpaceEx.h"
+#include "WonGradientFill/WonGradientFill.h"
+#include "WonSetThreadUILanguage/WonSetThreadUILanguage.h"
+#include "WonTransparentBlt/WonTransparentBlt.h"
